9e24bfe0cce2f297bad4555aadbf7175bd3f8d5a
Илья Сапсырин <nrgentoo@gmail.com>
2015-05-03 22:38:55 +0300

    Splash activity added

---
+++ app/src/main/java/com/nrgentoo/tedtalks/tedtalks/view/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.nrgentoo.tedtalks.tedtalks.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nrgentoo.tedtalks.tedtalks.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    // --------------------------------------------------------------------------------------------
+    //      LIFECYCLE
+    // --------------------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
